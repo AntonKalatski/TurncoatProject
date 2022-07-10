3aa83f34e990232d7ac776c25d92cf1d6c07a758
Anton Kalatski <kalatski24121991@gmail.com>
2022-07-10 17:54:04 +0300

    MouseRaycaster Plane/vs Physycs plane

--- Assets/Scripts/RayCaster/MouseRaycaster.cs
+++ Assets/Scripts/RayCaster/MouseRaycaster.cs
@@ -5,11 +5,17 @@ namespace RayCaster
     public class MouseRaycaster : MonoBehaviour
     {
         [SerializeField] private Transform debugPoint;
+        [SerializeField] private string[] layers;
+        [SerializeField] private LayerMask layerMask;
+        [SerializeField, Range(0, 100f)] private float rayCastLenght = 50f;
         private Camera _camera;
+        private Plane _plane;
 
         //todo camera provider
         private void Awake()
         {
+            layerMask = LayerMask.GetMask(layers);
+            _plane = new Plane(Vector3.up, Vector3.zero);
             _camera = Camera.main;
         }
 
@@ -18,8 +24,21 @@ namespace RayCaster
             var mousePos = Input.mousePosition;
             Ray ray = _camera.ScreenPointToRay(mousePos);
 
-            if (Physics.Raycast(ray, out RaycastHit info))
-                debugPoint.position = info.point;
+            if (_plane.Raycast(ray, out float enter))
+            {
+                debugPoint.position = ray.GetPoint(enter);
+            }
+        }
+
+        private void FakeUpdate()
+        {
+            var mousePos = Input.mousePosition;
+            Ray ray = _camera.ScreenPointToRay(mousePos);
+
+            if (Physics.Raycast(ray, out RaycastHit info, rayCastLenght, layerMask))
+            {
+                debugPoint.position = ray.GetPoint(rayCastLenght);
+            }
         }
     }
 }
